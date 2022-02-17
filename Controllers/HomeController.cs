8fa1ca1a7c3592016599699434624242d65aec59
James Peterson <jamespetersonwrestling@gmail.com>
2022-02-16 21:44:13 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission7.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext BookContext { get; set; }
+
+        public HomeController(BookstoreContext x)
+        {
+            BookContext = x;
+        }
+
+        public IActionResult Index()
+        {
+
+            var applications = BookContext.Books.ToList();
+
+            return View(applications);
+        }
+
+    }
+}
