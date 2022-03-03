1e2bbc1c49343540061a0414a5bebf3db13b088c
James Peterson <jamespetersonwrestling@gmail.com>
2022-03-03 14:46:52 -0700

    and more

--- Models/Basket.cs
+++ Models/Basket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -48,6 +49,7 @@ namespace Mission7.Models
 
     public class BasketLineItem
     {
+        [Key]
         public int LineID { get; set; }
         public Books Book { get; set; }
         public int Quantity { get; set; }
